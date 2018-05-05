e3f70d20aad1c6651d5f2fb140002336c9b3bf3f
Joseph Parker <jparker010190@gmail.com>
2018-05-05 17:37:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,9 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A[aeiou]/i) == nil
-      return false
-    else
-      return true
-    end
+if word.match(/^[aeiou]/)
+return  true
+else
+  return false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
